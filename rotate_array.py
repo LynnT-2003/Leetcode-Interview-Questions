5fccff49fa9ab964569324cb79d5ffb289e1995e
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-04-14 03:40:53 +0700

    Completed Rotate Array by "k" steps.

--- rotate_array.py
+++ rotate_array.py
@@ -1,13 +1,30 @@
-def rotate(nums):
-    print()
+
+# def rotate(nums, k):
+#     for i in range(k):
+#         nums.insert(0, nums.pop())
+#     print(nums)
+#     return(nums)
+
+def rotate(nums, k):
+    k = k % len(nums)
+    l, r = 0, len(nums) - 1
+    while l < r:
+        nums[l], nums[r] = nums[r], nums[l]
+        l, r = l + 1, r - 1
+
+    l, r = 0, k - 1
+    while l < r:
+        nums[l], nums[r] = nums[r], nums[l]
+        l, r = l + 1, r - 1
+
+    l, r = k, len(nums) - 1
+    while l < r:
+        nums[l], nums[r] = nums[r], nums[l]
+        l, r = l + 1, r - 1
+
     print(nums)
-    print()
-    k = 3
-    for i in range(k):
-        nums.insert(0, nums.pop())
-    print(nums)
+    return (nums)
 
 
-nums = [1, 2, 3, 4, 5, 6, 7]
-k = 3
-rotate(nums)
+nums = [-1, -100, 3, 99]
+rotate(nums, 2)
